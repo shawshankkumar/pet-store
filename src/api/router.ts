85986ea823a1dacd44cf1c5830213f3af9c01807
Shashank Kumar <shashankkumarthakur@gmail.com>
2021-02-21 17:01:50 +0530

    ✨ show pet feat

--- src/api/router.ts
+++ src/api/router.ts
@@ -1,16 +1,18 @@
 import { Router, Request, Response } from 'express';
-import { showAll, addPet } from './controller';
+import { showAllPets, addPet, show, update } from './controller';
 
 const app = Router();
 
 export const routerHandler = () => {
-  app.get('/showall', showAllHandler);
+  app.get('/showallpets', showAllPetsHandler);
   app.post('/add', addPetHandler);
+  app.get('/show', showHandler);
+  app.put('/updatepet', updateHandler);
   return app;
 };
 
-const showAllHandler = async (req: Request, res: Response) => {
-  showAll()
+const showAllPetsHandler = async (req: Request, res: Response) => {
+  showAllPets()
     .then(allPets => {
       res.json({ success: true, message: 'all pets displayed successfully', allPets });
     })
@@ -26,5 +28,25 @@ const addPetHandler = async (req: Request, res: Response) => {
     })
     .catch(error => {
       res.status(error.code).json({ success: false, message: error.message });
+    });
+};
+
+const showHandler = async (req: Request, res: Response) => {
+  show(req.body.owner as string)
+    .then(pet => {
+      res.json({ success: true, message: 'pet displayed successfully', pet: pet });
+    })
+    .catch(error => {
+      res.status(error.code).json({ message: error.message, success: false });
+    });
+};
+
+const updateHandler = async (req: Request, res: Response) => {
+  update(req.body, req.body.owner)
+    .then(() => {
+      res.json({ success: true, message: 'pet updated successfully' });
+    })
+    .catch(error => {
+      res.status(error.code).json({ message: error.message, success: false });
     });
 };
